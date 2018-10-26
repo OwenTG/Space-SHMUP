4a278327883e38d7765751f35273e0f41a19a3c5
OwenTG <42875734+OwenTG@users.noreply.github.com>
2018-10-26 16:50:01 -0400

    Eenemy_3 WIP
    Left off on line Vector3 v page 591

--- Assets/_Scripts/Enemy_3.cs
+++ Assets/_Scripts/Enemy_3.cs
@@ -2,15 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy_3 : MonoBehaviour {
+public class Enemy_3 : Enemy {
+    //Enemy_3 will move following a Bezier curve, which is a linear interpolation between more than two points
+    [Header("Set in Inspoector: Enemy_3")]
+    public float lifeTime = 5;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-}
+    [Header("Set Dynamically: Enemy_3")]
+    public Vector3[] points;
+    public float birthTime;
+
+    //Again, Start works well because it is not used by the Enemy superclass
+    void Start()
+    {
+        points = new Vector3[3]; //Initialize points
+
+        //The start position has already been set by Main.SpawnEnemy()
+        points[0] = pos;
+
+        //Set xMin and xMax the same way that Main.SpawnEnemy() does
+        float xMin = -bndCheck.camWidth + bndCheck.radius;
+        float Max = bndCheck.camWidth - bndCheck.radius;
+
+        Vector3 v;
+    }
+
+
